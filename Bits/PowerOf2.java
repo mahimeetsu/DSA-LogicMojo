ee21caa98c6e3ced73b3cb0084d1cf1b897790a5
mahimeetsu <130316103+mahimeetsu@users.noreply.github.com>
2025-10-15 09:43:54 -0500

    Update PowerOf2.java

--- Bits/PowerOf2.java
+++ Bits/PowerOf2.java
@@ -5,11 +5,26 @@ public class PowerOf2 {
 		int n = 2;
 		System.out.println(isPowerOfTwo(n));
 	}
-
+	//logn
+    public boolean isPowerOfTwo(int n) {
+        if(n==0)
+            return false;
+        while(n>0)
+        {
+            if(n==1)
+                return true;
+            if(n%2!=0)
+                break;
+            n=n/2;
+        }
+        return false;
+    }
+	//o(1)
 	public static boolean isPowerOfTwo(int n) {
 
        return n > 0 && (n & (n - 1)) == 0;
 
 	}
 }
+
 
