1c59b9d6d6f9b998581cc5234f768933c889f80f
mahimeetsu <130316103+mahimeetsu@users.noreply.github.com>
2026-02-14 07:43:48 -0600

    Refactor isPowerOfTwo method implementation
    Updated isPowerOfTwo method to use a loop for checking powers of two.

--- Bits/PowerOf2.java
+++ Bits/PowerOf2.java
@@ -19,6 +19,19 @@ public class PowerOf2 {
         }
         return false;
     }
+
+	=======================
+	//o(1)
+    public boolean isPowerOfTwo(int n) {
+        for (int i = 0; i < 31; i++) {
+            int ans = (int) Math.pow(2, i);
+            if (ans == n) {
+                return true;
+            }
+        }
+        return false;
+    }
+=========================
 	//o(1)
 	public static boolean isPowerOfTwo(int n) {
 
@@ -26,5 +39,6 @@ public class PowerOf2 {
 
 	}
 }
+
 
 
